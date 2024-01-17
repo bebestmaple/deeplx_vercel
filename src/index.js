3b00da5953d31265151eb2ef0c1abbc46bd12002
bebestmaple <447349864@qq.com>
2024-01-17 05:42:02 +0000

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import { Hono } from 'hono'
 import { query } from '@ifyour/deeplx'
 import { serve } from '@hono/node-server'
 
+const  path_guid = process.env.PATH_GUID;
+const translate_path =  path_guid ? `/${path_guid}/translate` : "/translate";
 const app = new Hono()
 
 app
   .get('/', (c) => c.text("OK"))
-  .get('/7d89775882674d9781eea0035a4bf3ef/translate', (c) => c.text('Please use POST method :)'))
-  .post('/7d89775882674d9781eea0035a4bf3ef/translate', async (c) => {
+  .get(translate_path, (c) => c.text('Please use POST method :)'))
+  .post(translate_path, async (c) => {
     const params = await c.req.json().catch(() => ({}))
     const result = await query(params)
     return c.json(result, result.code)
